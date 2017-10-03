344106ad7c53b52e7e6631ef78de45dbf6cfe8b7
Libertadoo <linda.vorobeja@inbox.lv>
2017-10-03 11:36:47 +0200

    fortuneteller

---
+++ script.js
@@ -0,0 +1,6 @@
+let name ="Linda";
+let numberOfChildren="2";
+let PartnersName ="someone";
+let home ="the Moon";
+let job ="media designer";
+console.log("My name is " + name + " and I will be a " +job +"." + " I will live on " +home + ". I will probobly be married to " +PartnersName + " and maybe we will have " + numberOfChildren + " kids.");
