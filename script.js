71938cb521382f52a8a4f82ca7395f6a52f156e4
Libertadoo <linda.vorobeja@inbox.lv>
2017-10-03 11:55:34 +0200

    MirgoKvadrats

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let name ="Linda";
-let numberOfChildren="2";
-let PartnersName ="someone";
-let home ="the Moon";
-let job ="media designer";
-console.log("My name is " + name + " and I will be a " +job +"." + " I will live on " +home + ". I will probobly be married to " +PartnersName + " and maybe we will have " + numberOfChildren + " kids.");
+let anim = document.querySelector("#anim");
+anim.addEventListener('click', startAnimation);
+
+function startAnimation(){
+    anim.classList.add('enable');
+}
